a6448c9afaac075d425ab946ecacc7202defea08
Austin Higgins <austinjhiggins@pm.me>
2025-01-27 12:29:00 -0330

    Started setup process from railway

---
+++ db/config.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const config = {
+    port: process.env.MYSQLPORT,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+  };
+  
+  module.exports = config;
+  
+  
+  
---
+++ db/db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+const config = require("./config");
+
+const connectDB = async () => {
+  const pool = mysql.createPool(config);
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log({ error: err.message });
+    }
+
+    console.log("Connected to MySQL database");
+    if (connection) connection.release();
+  });
+};
+
+module.exports = connectDB;
+
+
