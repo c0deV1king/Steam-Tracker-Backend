17675e8d903725e12b034ea70cd152a4a870152a
Austin Higgins <austinjhiggins@pm.me>
2025-01-28 16:27:25 -0330

    Removed problematic function

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
-const connection = require("./db/config");
 const port = process.env.PORT;
 
 const app = express();
@@ -10,8 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-connection();
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
