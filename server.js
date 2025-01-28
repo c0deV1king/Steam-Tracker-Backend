b24dbb76cfd0d38030d115562f42b9ba0adf41b5
Austin Higgins <austinjhiggins@pm.me>
2025-01-28 16:12:40 -0330

    changed variable name

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
-const connectDB = require("./db/db");
+const connection = require("./db/db");
 const port = process.env.PORT;
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-connectDB();
+connection();
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
