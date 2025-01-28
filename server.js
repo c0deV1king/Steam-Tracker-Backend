7325ac4219f24b7de92ef43b3fbc860b8f8c31e1
Austin Higgins <austinjhiggins@pm.me>
2025-01-28 16:44:58 -0330

    Fixed typo

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-connection.connect((eer) => {
+connection.connect((err) => {
   if (err) {
     console.error('Error connecting to database:', err);
     return;
