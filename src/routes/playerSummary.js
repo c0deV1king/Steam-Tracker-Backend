441ed638774d0a70c95507031c32982d399da853
c0dev1king <austinjhiggins@pm.me>
2025-02-05 18:20:12 -0330

    typo

--- src/routes/playerSummary.js
+++ src/routes/playerSummary.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const playerProfile = require('../src/getPlayerSummary')
+const playerProfile = require('../controllers/getPlayerSummary')
 
 let playerSummary = express.Router();
 
