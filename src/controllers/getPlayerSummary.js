8405484f0ae70cda87e49e07f19b970b1000db72
Austin Higgins <austinjhiggins@pm.me>
2025-02-06 10:12:48 -0330

    Changed export method

--- src/controllers/getPlayerSummary.js
+++ src/controllers/getPlayerSummary.js
@@ -1,12 +1,8 @@
 import fetch from 'node-fetch';
 
 
-const getPlayerSummary = async (req, res) => {
+export const getPlayerSummary = async (req, res) => {
   const response = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamApiKey}&steamids=76561198119786249`);
   const data = await response.json();
   console.log(data);
-};
-
-module.exports = {
-  getPlayerSummary
 };
