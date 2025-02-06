d3d9696535181d8c73f94761f8a913d2779cbe88
Austin Higgins <austinjhiggins@pm.me>
2025-02-06 10:25:24 -0330

    Fixing red errors

--- src/controllers/getPlayerSummary.js
+++ src/controllers/getPlayerSummary.js
@@ -1,8 +1,12 @@
 import fetch from 'node-fetch';
 
 
-export const getPlayerSummary = async (req, res) => {
+const getPlayerSummary = async (req, res) => {
   const response = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamApiKey}&steamids=76561198119786249`);
   const data = await response.json();
   console.log(data);
+};
+
+module.exports = {
+  getPlayerSummary
 };
