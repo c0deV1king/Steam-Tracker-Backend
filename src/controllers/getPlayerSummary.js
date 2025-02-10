98a65217b55dd9ad7a1ecbf94942ff9f9213dbef
c0dev1king <austinjhiggins@pm.me>
2025-02-10 12:11:46 -0330

    updated api call logic

--- src/controllers/getPlayerSummary.js
+++ src/controllers/getPlayerSummary.js
@@ -1,8 +1,25 @@
-
 const getPlayerSummary = async (req, res) => {
-  const response = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamApiKey}&steamids=76561198119786249`);
-  const data = await response.json();
-  console.log(data);
+  const steamApiKey = process.env.steamApiKey;
+  const steamid = process.env.steamid;
+
+  console.log('my steam id is: ', steamid);
+  console.log('my api key is: ', steamApiKey);
+  try {
+    const response = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamApiKey}&steamids=${steamid}`);
+    console.log("fetching data")
+    const data = await response.json();
+    console.log("fetched data as: ", JSON.stringify(data))
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: `Failed to fetch achievements for appid: ${appid}` })
+    };
+  }
 };
 
 module.exports = {
