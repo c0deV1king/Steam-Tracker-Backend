a8f386163a2853910e6b0edc15c770618420fa56
Austin <austinjhiggins@pm.me>
2025-04-07 13:56:14 -0230

    Painfully fixed the steam openid login issues

--- src/controllers/steamValidation.controller.ts
+++ src/controllers/steamValidation.controller.ts
@@ -13,16 +13,21 @@ export class steamValidationController {
 
   private initializeRoutes() {
     this.route.get("/", async (req: Request, res: Response) => {
+      console.log("Steam Validation Route Hit");
       try {
         console.log("Incoming Query Parameters:", req.query);
         const params = new URLSearchParams(req.query as any);
+        const backendUrl = process.env.BACKEND_URL || "http://localhost:3000";
 
+        const returnToUrl = `${backendUrl}/api/validate-steam/`;
+        console.log("Return To URL:", returnToUrl);
         // Validate the Steam response
         const validationResponse = await axios.post(
           "https://steamcommunity.com/openid/login",
           new URLSearchParams({
             ...Object.fromEntries(params),
             "openid.mode": "check_authentication",
+            "openid.return_to": returnToUrl,
           }),
           {
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -41,6 +46,10 @@ export class steamValidationController {
 
             const frontendUrl =
               process.env.FRONTEND_URL || "http://localhost:3001";
+            console.log(
+              "Redirecting to frontend URL:",
+              `${frontendUrl}/?steamId=${steamId}`
+            );
             res.redirect(`${frontendUrl}/?steamId=${steamId}`);
             console.log(
               "Redirecting to:",
